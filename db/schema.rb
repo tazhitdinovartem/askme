ff229072a26e57847eb9b594f62fa16c78cb4f75
artem <tazhitdinov.artem@gmail.com>
2020-01-26 12:59:41 +0500

    Added index to DB for Users Username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_26_072955) do
+ActiveRecord::Schema.define(version: 2020_01_26_075645) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_01_26_072955) do
     t.string "password_salt"
     t.string "avatar_url"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "questions", "users"
