c38d6d3974255504a047535ad70122820d19454a
Artem Tazhitdinov <nemesisrise@gmail.com>
2020-02-01 13:49:12 +0500

    Added log-in after sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
 
     if @user.save
       redirect_to root_url, notice: "Вы успешно зарегистрировались." 
+      session[:user_id] = @user.id
     else
       render 'new'
     end
