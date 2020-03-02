07db95d60ddd94470702edeee0a225c36509cffc
ArtemTazhitdinov <tazhitdinov.artem@gmail.com@>
2020-03-02 19:33:44 +0500

    reCaptcha added

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,7 +9,7 @@ class QuestionsController < ApplicationController
     @question = Question.new(question_params)
     @question.author = current_user
 
-    if @question.save
+    if check_captcha(@question) && @question.save
       redirect_to user_path(@question.user), notice: 'Вопрос задан'
     else
       render :edit
@@ -46,5 +46,9 @@ class QuestionsController < ApplicationController
 
     def authorize_user
       reject_user unless @question.user == current_user  
+    end
+
+    def check_captcha(model)
+      current_user.present? || verify_recaptcha(model: model)
     end
 end
